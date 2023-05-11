d309ef6734d8c55813d8f249e3ceb99b4867e63c
Elindanova <bonamy.antoine@hotmail.fr>
2023-05-11 17:23:59 +0200

    Save commit

--- frontend/src/components/ToDoList/MandatoryList.jsx
+++ frontend/src/components/ToDoList/MandatoryList.jsx
@@ -2,14 +2,7 @@ import { useEffect, useState } from "react";
 
 function MandatoryList() {
   const [data, setData] = useState([]);
-  const mandatoryList = [
-    "Passeport/Carte d'identité",
-    "VISA",
-    "Argent",
-    "Vaccins",
-    "Valise",
-    "Billet(s)",
-  ];
+  const [pays] = useState({});
 
   useEffect(() => {
     fetch("http://localhost:5025/api/pays")
@@ -20,9 +13,34 @@ function MandatoryList() {
       });
   }, []);
 
+  const mandatoryList = ["Billet(s) de transport", "la valise, bien sur !"];
+
   return (
     <div className="mand-list">
+      <h2>Pays Choisi : {pays.pays}</h2>
+      <br />
       <h3>A ne pas oublier !</h3>
+      <ul>
+        <div className="mand-list-box">
+          <input type="checkbox" />
+          <li>
+            {pays.passeport === 1 ? "Passeport" : "Carte d'identité/Passeport"}
+          </li>
+          <br />
+        </div>
+        <div className="mand-list-box">
+          <input type="checkbox" />
+          <li>VISA : {pays.visa}</li>
+        </div>
+        <div className="mand-list-box">
+          <input type="checkbox" />
+          <li>prix du VISA : {pays.prix} euros</li>
+        </div>
+        <div className="mand-list-box">
+          <input type="checkbox" />
+          <li>Vaccins : {pays.vaccin}</li>
+        </div>
+      </ul>
       <ul>
         {mandatoryList.map((el) => {
           return (
