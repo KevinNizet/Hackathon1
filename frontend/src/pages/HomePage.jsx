2b2f829879ef6577579365159d49c3231af465fd
chavanne.marion@gmail.com <chavanne.marion@gmail.com>
2023-05-11 14:29:47 +0200

    Ajout structure card et selecteur en attente de l'API

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import NavBar from "../components/NavBar";
+import Card from "../components/Card";
+import SelectBar from "../components/SelectBar";
 
 function HomePage() {
   return (
     <div>
       <NavBar />
+      <SelectBar />
+      <Card />
       <p>Hello HomePage</p>
     </div>
   );
