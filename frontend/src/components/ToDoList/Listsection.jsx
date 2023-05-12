45444232955a3269155df509dcf7122bc74b6a3e
Tomtoxi44 <tommy.angibaud@gmail.com>
2023-05-12 12:30:18 +0200

    Ajout du bouton validation de la todolist

--- frontend/src/components/ToDoList/Listsection.jsx
+++ frontend/src/components/ToDoList/Listsection.jsx
@@ -1,14 +1,27 @@
+import { useState } from "react";
 import NavBar from "../NavBar";
 import MandatoryList from "./MandatoryList";
 import OptionalList from "./OptionalList";
+import Validation from "./Validation";
 
 function ListSection() {
+  const [tumevoie, settumevoieplus] = useState(undefined);
+  const handleClick = () => {
+    return settumevoieplus(true);
+  };
+
   return (
     <>
       <NavBar />
       <MandatoryList />
       <br />
       <OptionalList />
+      <div className="buttonContainer">
+        <button type="button" onClick={handleClick} className="buttonValider">
+          Valider
+        </button>
+        {tumevoie && <Validation settumevoieplus={settumevoieplus} />}
+      </div>
     </>
   );
 }
